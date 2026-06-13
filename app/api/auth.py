4cc00b99226aa5b96ba94d33efe9e32a88c0c3c5
pedropauloportella <ppportella@gmail.com>
2026-06-13 19:37:46 -0300

    Orcid login

--- app/api/auth.py
+++ app/api/auth.py
@@ -58,6 +58,7 @@ async def orcid_callback(code: str, db: Session = Depends(get_db)):
                 "client_id": settings.ORCID_CLIENT_ID,
                 "client_secret": settings.ORCID_CLIENT_SECRET,
                 "grant_type": "authorization_code",
+                "redirect_uri": f"http://localhost:8000{settings.API_V1_STR}/auth/orcid/callback",
                 "code": code,
             },
             headers={"Accept": "application/json"}
