553d8429aa94bf383c8208b59e9bfd5b32e943ac
pedropauloportella <ppportella@gmail.com>
2026-06-14 18:25:31 -0300

    Atualizar a classe base para usar UUID do dialeto PostgreSQL, garantindo a geração automática de IDs no servidor.

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,13 +1,13 @@
 import uuid
-from sqlalchemy import Column, UUID
+from sqlalchemy import Column, text
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 @as_declarative()
 class Base:
     # Classe base declarativa para todos os modelos do SQLAlchemy.
-    # Como não usamos SQLite, usamos o UUID nativo do SQLAlchemy que mapeia para UUID no Postgres.
-    # default=uuid.uuid4 garante a geração automática no servidor de aplicação.
-    id = Column(UUID, primary_key=True, default=uuid.uuid4, index=True)
+    # Usamos o UUID do dialeto PostgreSQL para garantir que o Alembic gere 'UUID' e não 'INTEGER'
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"), index=True)
     __name__: str
 
     @declared_attr
